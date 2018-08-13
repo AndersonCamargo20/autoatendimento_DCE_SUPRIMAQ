25ac0763df98161b303b73d17fde65f4b5c3d5e4
Andeson de Camargo <anderson.camargo@unochapeco.edu.br>
2018-08-13 16:46:39 -0300

    review

--- app/controllers/use_case_controller.rb
+++ app/controllers/use_case_controller.rb
@@ -2,24 +2,18 @@ class UseCaseController < ApplicationController
   require 'jwt'
   def newUser
     user = User.new
-    if request.headers['HTTP_AUTORIZATION'].blank?
+    if request.headers['HTTP_AUTHORIZATION'].blank?
       render :json => "Unauthorized", :status => 401
     else
-      if request.headers['HTTP_AUTORIZATION'] != "Eb 14548251"
+      if request.headers['HTTP_AUTHORIZATION'] != "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb21wYW55X2lkIjoxOH0.yRP59sRufpm9ro6RGZ8nuZcfRVMKqkCvneBz6KZB4mU"
         render :json => "Forbiden_Autorization", :status => 403
       else
         if request.headers['HTTP_PASSWORD'].blank? || request.headers['HTTP_EMAIL'].blank?  || request.headers['HTTP_NOME'].blank?
           render :json => "Not Found", :status => 404
         else
-          hmac_secret = request.headers['HTTP_AUTORIZATION']
-          access = {
-            email: request.headers['HTTP_EMAIL'],
-            password: request.headers['HTTP_PASSWORD'],
-            autorization: hmac_secret,
-            session: DateTime.current
-          }
-
-          token_access = JWT.encode access, hmac_secret
+          hmac_secret = request.headers['HTTP_AUTHORIZATION']
+          
+          
 
           password = {
             password: request.headers['HTTP_PASSWORD']
@@ -27,23 +21,16 @@ class UseCaseController < ApplicationController
 
 
           password_coded = crypteParams(password, hmac_secret)
-          #user.nome = request.headers['HTTP_NOME']
-          #user.email = request.headers['HTTP_EMAIL']
-          #user.password = hash_params[:password]
-          #token_pass = JWT.encode request.headers['HTTP_PASSWORD'], nil, false
           user = User.create(nome: request.headers['HTTP_NOME'], email: request.headers['HTTP_EMAIL'], password: password_coded, credit: "0", created_at: DateTime.current, updated_at: DateTime.current)
-          #user.credit = 0
-          #user.save
-          #return_hash  = [
-           # name: user.nome,
-            #email: user.email,
-            #credit: user.credit,
-          #  access_token: token_access
-          #]
+          
+          access = {
+            email: request.headers['HTTP_EMAIL'],
+            password: password_coded,
+            session: DateTime.current
+          }
+          token_access = JWT.encode access, hmac_secret
 
-          #email_decoded = JWT.decode(user.password, hmac_secret, "H256")
-          #render :json => return_hash
-          render :json => "OK", :status => 200
+          render :json => access, :status => 200
         end
       end
     end
